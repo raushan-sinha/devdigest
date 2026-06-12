fc8e64ddedaa8092c43b79cf9904755638007652
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-12 16:23:55 +0530

    Created a folder for Data container & added a file for Navbar links

---
+++ data/navbarLinks.ts
@@ -0,0 +1,24 @@
+import { NavbarLinksProps } from "@/types/navbar.types";
+
+export const NavLinks: NavbarLinksProps[] = [
+    {
+        id: 1,
+        title: "Home",
+        link: "/"
+    },
+    {
+        id: 2,
+        title: "Tech News",
+        link: "/tech-news",
+    },
+    {
+        id: 3,
+        title: "AI",
+        link: "/ai",
+    },
+    {
+        id: 4,
+        title: "Jobs",
+        link: "/jobs",
+    },
+]
