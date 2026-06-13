dae6772d7b486d4c768c1d0f8eeee96cb935e132
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-13 16:06:42 +0530

    Added logic for Close & Open Icon

--- components/layout/MobileNavbar.tsx
+++ components/layout/MobileNavbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { BiMenu } from "react-icons/bi";
+import { RxCross2 } from "react-icons/rx";
 
 export default function MobileNavbar() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
     return (
         <header className="border-b">
             <nav className="mx-auto flex items-center justify-between px-4 py-4 sm:px-6">
@@ -17,9 +23,17 @@ export default function MobileNavbar() {
                 <button
                     type="button"
                     aria-label="Open Menu"
-                    className="flex h-10 w-10 items-center justify-center rounded-lg border transition-colors hover:bg-gray-100"
+                    className="relative z-50 flex h-10 w-10 items-center justify-center border-2 rounded-xl"
+                    onClick={() => {
+                        console.log('Clicked');
+                        setMenuOpen(!menuOpen)
+                    }}
                 >
-                    <BiMenu size={22} />
+                    {menuOpen ? (
+                        <RxCross2 size={30} />
+                    ) : (
+                        <BiMenu size={30} />
+                    )}
                 </button>
             </nav>
         </header>
