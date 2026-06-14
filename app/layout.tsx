d161df2b285c818d722d62b943a36f575e6e1e21
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-14 14:17:02 +0530

    Updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             lang="en"
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
-            <body className="min-h-full flex flex-col">
+            <body className="min-h-full flex flex-col bg-gradient-to-br from-indigo-50 via-white to-cyan-100">
                 <header>
                     <div className="hidden md:block">
                         <Navbar />
