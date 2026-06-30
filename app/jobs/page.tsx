e0281c34ba82f99ea92cd542ffb7b231462deffe
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-30 22:56:59 +0530

    Added logic for consoling Jobs API for testing

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,4 +1,23 @@
+'use client';
+import { useEffect } from "react";
+
 export default function Jobs() {
+    // Fetch Jobs Potsing API -
+    const jobsFetching = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/jobs/api');
+            if(!response.ok) throw new Error('Failed to fetch API');
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        jobsFetching();
+    }, []);
+
     return (
         <h1>Jobs Page</h1>
     );
