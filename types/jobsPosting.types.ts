3a5464f22f3bc4c2b104f3582da82403a8768de0
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-01 20:06:08 +0530

    Added types safety for Jobs Posting API data

---
+++ types/jobsPosting.types.ts
@@ -0,0 +1,10 @@
+export interface JobsDataProps {
+    id: number,
+    url: string,
+    title: string,
+    company_name: string,
+    category: string,
+    job_type: string,
+    candidate_required_location: string,
+    salary: string,
+}
