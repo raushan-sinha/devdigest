8d08dd99657a6a6a55f5d0d65ad627e83878e4bc
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-04 13:32:29 +0530

    Removed console code

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -10,7 +10,6 @@ export default function Jobs() {
     const jobsFetching = async () => {
         try {
             const data = await getJobsPostingData();
-            console.log(data);
             setJobsData(data);
         } catch (error) {
             console.error(error)
