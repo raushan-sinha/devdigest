f7d6f7c1c6ba71ce84b4ea5d0604a18d668d8096
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-08 20:39:48 +0530

    Added data for Platform Links

---
+++ data/footerLinks.ts
@@ -0,0 +1,9 @@
+import { FooterLinkProps } from "@/types/footerLinks.types";
+
+// Platform Links -
+export const PLATFORM_LINKS: FooterLinkProps[] = [
+    { id: 1, url: '/', name: 'Home' },
+    { id: 2, url: '/tech-news', name: 'Tech News' },
+    { id: 3, url: '/jobs', name: 'Jobs' },
+    { id: 4, url: '/ai', name: 'AI' }
+]
