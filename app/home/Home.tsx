1f43c4bf228e0a5aedafdfd9a79ac1bf84125492
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-12 14:22:34 +0530

    Added component - ScrollDownButton

--- app/home/Home.tsx
+++ app/home/Home.tsx
@@ -1,11 +1,14 @@
+import ScrollDownButton from "@/components/ui/ScrollDownButton";
 import { HeroSection, JobsSection, TechNewsSection } from "./sections";
 
 export default function Home() {
-    return (
-        <>
-            <HeroSection />
-            <TechNewsSection />
-            <JobsSection />
-        </>
-    )
+  return (
+    <>
+      <ScrollDownButton />
+
+      <HeroSection />
+      <TechNewsSection />
+      <JobsSection />
+    </>
+  );
 }
