b2d7c5a6f90e0dbd7b4bac423899ab6cf9312bd9
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-12 23:24:51 +0530

    Added types safety for Social Links

--- types/footerLinks.types.ts
+++ types/footerLinks.types.ts
@@ -1,14 +1,16 @@
 import { IconType } from "react-icons"
 
+// Social Links -
+export interface SocialLinksProps {
+    id: number,
+    icon: IconType,
+    url: string,
+    size: number
+}
+
 // Footer Links -
 export interface FooterLinkProps {
     id: number,
     url: string,
     name: string
-}
-
-// Social Links -
-export interface SocialLinksProps {
-    icon: IconType,
-    url: string
 }
