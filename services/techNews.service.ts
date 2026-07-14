b42b2d0f19ab38b51877d0092d3be10073a433f2
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-14 20:22:58 +0530

    Added logic for Tech News Props types safety

--- services/techNews.service.ts
+++ services/techNews.service.ts
@@ -1,10 +1,11 @@
+import { TechNewsProps } from "@/types/techNews.types";
 import axios from "axios"
 
 const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_TECH_NEWS_BACKEND_URL
 });
 
-export const getTechNews = async () => {
-    const response = await API.get('');
+export const getTechNews = async (): Promise<TechNewsProps[]> => {
+    const response = await API.get<TechNewsProps[]>('');
     return response.data;
 }
