6d16a8ba01be7fe8871b6b6ec7f242d801b1f0f8
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-19 20:38:59 +0530

    Added Card component

--- app/react-ecosystem/sections/ReactRoadmap.tsx
+++ app/react-ecosystem/sections/ReactRoadmap.tsx
@@ -1,3 +1,4 @@
+import RoadmapCard from "@/components/ui/RoadmapCard";
 import { REACT_ROADMAP_DATA } from "@/data/reactRoadmap.data";
 
 export default function ReactRoadmap() {
@@ -40,22 +41,7 @@ export default function ReactRoadmap() {
 
                     <div className="space-y-10">
                         {REACT_ROADMAP_DATA.map((item) => (
-                            <div key={item.id} className={`group relative rounded-3xl border ${item.borderColor} bg-gradient-to-br ${item.bgColor} p-8 backdrop-blur-xl transition duration-300 hover:border-cyan-400 hover:shadow-[0_20px_60px_rgba(34,211,238,.15)] lg:ml-20`}>
-
-                                <div className={`absolute -left-[74px] hidden lg:flex h-14 w-14 items-center justify-center rounded-full border-4 border-slate-950 ${item.stepColor} text-xl font-bold text-white`}>
-                                    {item.id}
-                                </div>
-
-                                <span className={`inline-flex rounded-full bg-cyan-500/20 px-3 py-1 text-xs font-semibold uppercase tracking-wide ${item.tagColor}`}>
-                                    {item.tag}
-                                </span>
-
-                                <h3 className="mt-5 text-3xl font-bold text-white">
-                                    {item.title}
-                                </h3>
-
-                                <p className="mt-4 text-slate-300 leading-7">{item.description}</p>
-                            </div>
+                            <RoadmapCard key={item.id} {...item} />
                         ))}
                     </div>
 
