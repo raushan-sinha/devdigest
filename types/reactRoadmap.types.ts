9747879de9778883609d06c21778ac9b16814a1a
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-19 14:46:28 +0530

    Added types safety for React Roadmap Data

---
+++ types/reactRoadmap.types.ts
@@ -0,0 +1,12 @@
+export interface REACT_ROADMAP_PROPS {
+    id: number,
+    tag: string,
+    title: string,
+    description: string
+    borderColor: string
+    bgColor: string
+    hoverColor: string
+    badgeColor: string
+    tagColor: string
+    stepColor: string
+}
