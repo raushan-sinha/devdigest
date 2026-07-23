ff531c156b30151cda084067860c3de5d0df8488
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-23 20:27:55 +0530

    Added a component - QuotePopupCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import QuotePopupCard from "@/components/ui/QuotePopupCard";
 import Home from "./home/Home";
 
 export default function MainPage() {
     return (
         <>
             <Home />
+
+            <QuotePopupCard />
         </>
     );
 }
