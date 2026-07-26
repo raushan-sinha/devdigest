4ef8ad5e3126cd501d2ed488d97d53a099c192df
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-26 14:28:49 +0530

    Added lofic for showing or hiding Popup Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
+'use client';
 import QuotePopupCard from "@/components/ui/QuotePopupCard";
 import Home from "./home/Home";
+import { useEffect, useState } from "react";
 
 export default function MainPage() {
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPopup(true);
+        }, 300);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleClose = () => {
+        setShowPopup(false);
+    }
+
     return (
         <>
             <Home />
 
-            <QuotePopupCard />
+            {showPopup && (
+                <QuotePopupCard onClose={handleClose} />
+            )}
         </>
     );
 }
