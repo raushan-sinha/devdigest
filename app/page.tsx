d61589eb06f20d377a2bac59b465166be37e6f31
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-07-30 20:37:35 +0530

    Updated logic for showing Quote Data on UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,27 @@
 import QuotePopupCard from "@/components/ui/QuotePopupCard";
 import Home from "./home/Home";
 import { useEffect, useState } from "react";
+import { getQuoteUrlData } from "@/services/quoteUrl.services";
+import { QuoteData } from "@/types/quoteCardProps.types";
 
 export default function MainPage() {
     const [showPopup, setShowPopup] = useState<boolean>(false);
+    const [showQuote, setShowQuote] = useState<QuoteData[]>([]);
+
+    // todo: Handle Quote Url -
+    useEffect(() => {
+        const loadQuoteData = async () => {
+            try {
+                const quoteResponse = await getQuoteUrlData();
+                console.log(quoteResponse);
+                setShowQuote(quoteResponse);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        loadQuoteData();
+    }, []);
 
     // todo: Handle the Pop up Quote Card -
     useEffect(() => {
@@ -24,8 +42,8 @@ export default function MainPage() {
         <>
             <Home />
 
-            {showPopup && (
-                <QuotePopupCard onClose={handleClose} />
+            {showPopup && showQuote.length > 0 && (
+                <QuotePopupCard onClose={handleClose} q={showQuote[0].q} a={showQuote[0].a} />
             )}
         </>
     );
